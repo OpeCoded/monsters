f81c1885bbf0fa4a7c5505270e45e0b71c46c92e
Ademoye <opeyboy4real@gmail.com>
2019-07-29 12:11:48 -0400

    Updates

---
+++ precache-manifest.0ef56130674ccfaaae05af5d80d9ea8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3f70494b3a2a917f78fefc8610d603",
+    "url": "/OpeCoded/monsters/index.html"
+  },
+  {
+    "revision": "848f58909f8b729d6dc1",
+    "url": "/OpeCoded/monsters/static/css/main.cc6b26b9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/OpeCoded/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "848f58909f8b729d6dc1",
+    "url": "/OpeCoded/monsters/static/js/main.c78b39bc.chunk.js"
+  },
+  {
+    "revision": "678a1a546bd42a1e2b49",
+    "url": "/OpeCoded/monsters/static/js/runtime~main.b703c8df.js"
+  }
+]);
